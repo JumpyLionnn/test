f622432c4c902b8de32edc62a88fe2d872ec4582
JumpyLionnn <jumpylionn1@gmail.com>
2023-01-03 19:18:06 +0200

    test

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+//import { handler } from '../../frontend/build/handler.js';
+import express from 'express';
+
+const cwd = process.cwd();
+async function main(){
+    const {handler} = await import("file://" + cwd + "/frontend/build/handler.js");
+    const app = express();
+
+    // add a route that lives separately from the SvelteKit app
+    app.get('/healthcheck', (req, res) => {
+        res.end('ok');
+    });
+    
+    // let SvelteKit handle everything else, including serving prerendered pages and static assets
+    app.use(handler);
+    
+    app.listen(3000, () => {
+        console.log('listening on port 3000');
+    });
+}
+
+main();
