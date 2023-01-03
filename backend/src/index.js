3023959ef9fbb444fa110dd8f0a89edd6f1c2add
JumpyLionnn <jumpylionn1@gmail.com>
2023-01-03 21:33:50 +0200

    Limit the memory of the scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ async function main(){
     // let SvelteKit handle everything else, including serving prerendered pages and static assets
     app.use(handler);
     
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log('listening on port 3000');
     });
 }
